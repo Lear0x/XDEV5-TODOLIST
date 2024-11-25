f1c1e974cfab783b8c18ad785c5ba248fa20ad53
ValentinChretien <valentin.chretien@cegape.fr>
2024-11-25 16:04:39 +0100

    Connection MongoDB + Petits ajustements

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { createUser, getUserById } from '../controllers/user.controller';
 
 const router = Router();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,14 +4,25 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
 import todoItemRoutes from './routes/todoItem.routes';
 import todoListRoutes from './routes/todoList.routes';
+import mongoose from 'mongoose';
 
 // Charger les variables d'environnement
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI || 'mongodb://admin:pass@mongo:27017?authSource=admin';
 
 app.use(express.json());
+
+// Connexion à MongoDB
+mongoose
+    .connect( mongoURI )
+    .then( () => {
+        const db = mongoose.connection.useDb('todolist');
+        console.log('MongoDB connected'); 
+    })  
+    .catch( (error) => console.error('Error connecting to MongoDB:', error) );
 
 app.use('/api/users', userRoutes);
 app.use('/api/todo-items', todoItemRoutes); // Routes pour les TodoItems
